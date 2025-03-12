8deee9e4503e2d433738583a0e605b9a1191ad7b
Michael Fischer <michael.fischer@delegate-group.com>
2025-03-12 12:20:57 +0100

    Enhance PDF merging functionality to support encrypted files and update requirements

--- MergePDFs.py
+++ MergePDFs.py
@@ -1,16 +1,20 @@
 from glob import glob
-from PyPDF2 import PdfMerger
+from PyPDF2 import PdfMerger, PdfReader
 
-
-
-def pdf_merge():
+def pdf_merge(password):
     ''' Merges all the pdf files in current directory '''
     merger = PdfMerger()
     allpdfs = [a for a in glob("*.pdf")]
-    [merger.append(pdf) for pdf in allpdfs]
+    
+    for pdf in allpdfs:
+        reader = PdfReader(pdf)
+        if reader.is_encrypted:
+            reader.decrypt(password)
+        merger.append(reader)
+    
     with open("Merged_pdfs.pdf", "wb") as new_file:
         merger.write(new_file)
 
-
 if __name__ == "__main__":
-    pdf_merge()
+    password = input("Enter the password for the PDFs: ")
+    pdf_merge(password)
